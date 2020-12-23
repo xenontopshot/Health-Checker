2fa8ca6dcca624a96572c02c53301691689d71c3
ilmi <ilmir311@gmail.com>
2020-12-23 12:35:58 +0800

    first commit, main menu UI

---
+++ app/src/main/java/mmu/edu/my/healthchecker/hospital.java
@@ -0,0 +1,14 @@
+package mmu.edu.my.healthchecker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class hospital extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hospital);
+    }
+}
